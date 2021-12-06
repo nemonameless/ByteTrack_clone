65c3f6cdc152156c439bc07b76331b31cf5163fd
Qtieee <tungdop2@gmail.com>
2021-12-06 15:20:41 +0700

    Update demo_track_yolov5.sh

--- demo_track_yolov5.sh
+++ demo_track_yolov5.sh
@@ -8,14 +8,19 @@ then
 	# unzip
     unzip -q MOT17.zip -d datasets/
     # rename folder
-    !mv datasets/MOT17 datasets/mot
+    mv datasets/MOT17 datasets/mot
     # remove zip
-    !rm -r MOT17.zip
+    rm -r MOT17.zip
 fi
 
 for i in MOT17-02-FRCNN MOT17-04-FRCNN MOT17-05-FRCNN MOT17-09-FRCNN MOT17-10-FRCNN MOT17-11-FRCNN MOT17-13-FRCNN
+do
 	(
 		# run inference on sequence frames
 		python3 tools/demo_track_yolov5.py --path ./datasets/mot/train/$i/img1 image -f exps/example/mot/yolox_x_mot17_half.py -c pretrained/bytetrack_x_mot17.pth.tar --fp16 --fuse --save_result --save_name $i
 	)
 done
+
+# (all need to be finished)
+wait
+echo "Inference on all MOT17 sequences DONE"
