60b0e02391a378de466c9902253374f9dbea8b24
Qtieee <tungdop2@gmail.com>
2021-12-14 14:16:47 +0700

    Revert "update"
    This reverts commit 672656392064a0593d758af79fb25c89762a5970.

--- exps/example/mot/yolov5_s_mix_det.py
+++ exps/example/mot/yolov5_s_mix_det.py
@@ -12,8 +12,8 @@ class Exp(MyExp):
     def __init__(self):
         super(Exp, self).__init__()
         self.num_classes = 1
-        self.depth = 0.67
-        self.width = 0.75
+        self.depth = 1.33
+        self.width = 1.25
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
         self.train_ann = "train.json"
         self.val_ann = "train.json"    # change to train.json when running on training set
@@ -23,7 +23,7 @@ class Exp(MyExp):
         self.max_epoch = 80
         self.print_interval = 20
         self.eval_interval = 5
-        self.test_conf = 0.001
+        self.test_conf = 0.1
         self.nmsthre = 0.7
         self.no_aug_epochs = 10
         self.basic_lr_per_img = 0.001 / 64.0
